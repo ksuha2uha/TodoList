03c6bc0726d40a7aa893d530316d2880486d47d8
Kseniya <ksuha2uha@MacBook-Air-Kseniy.local>
2020-02-11 20:55:16 +0200

    init

---
+++ precache-manifest.14534a6c52228483ffdd5c78581f4ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad78ad65000f7060bf30f750a19fe0c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b91bd2b34a350e2b9657",
+    "url": "/static/css/main.e4a4a2ed.chunk.css"
+  },
+  {
+    "revision": "532a3c6b7a2b83751e6b",
+    "url": "/static/js/2.8e9e2458.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8e9e2458.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b91bd2b34a350e2b9657",
+    "url": "/static/js/main.1a7b26db.chunk.js"
+  },
+  {
+    "revision": "452efe56c2036ff11208",
+    "url": "/static/js/runtime-main.83c66be5.js"
+  }
+]);
